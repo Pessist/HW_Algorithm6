a093d1782cb2ed300fe4defb8b14eb6373af0410
Anton <pessist@rambler.ru>
2021-03-13 22:25:00 +0300

    Add project files.

---
+++ HW_Algorithm6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_Algorithm6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
